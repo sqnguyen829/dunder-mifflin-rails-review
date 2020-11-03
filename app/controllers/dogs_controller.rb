1465638817c28e93810505ada355e9328c502ea3
sqnguyen829 <sqnguyen829@gmail.com>
2020-11-03 12:50:26 -0600

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,20 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+
+        if session 
+            @dogs = @dogs.sort {|dog1, dog2| dog2.employees.count <=> dog1.employees.count}
+        end
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def sort_dogs
+        @dogs =  Dog.all
+        @dogs = @dogs.sort {|dog1, dog2| dog2.employees.count <=> dog1.employees.count}
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates_uniqueness_of :alias, condition: -> {where.not(alias: 'none')}
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
 end
